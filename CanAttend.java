a49eeb5e2360e203dcdf948b94a4f63b907c4f0d
coolight543 <robamiao@gmail.com>
2026-02-11 13:38:20 +0000

    sleiiwft

--- CanAttend.java
+++ CanAttend.java
@@ -2,12 +2,24 @@ import java.util.ArrayList;
 
 public class CanAttend {
 
-	//precondition: 
-	//postcondition: 
+	//precondition: The arraylists are not null
+	//postcondition: should print out what meetings cannot attend
 	public static boolean canAttend(ArrayList<MeetingInterval> meetings) {
 		//your implementation here
-		System.out.println(meetings);
-		return false; //replace me with actual logic please!
+		for (MeetingInterval second : meetings)
+		{
+			for (MeetingInterval third : meetings)
+			{
+				// make sure the third.getStart() is between second.getStart and second.getEnd
+				if (second.getEnd() > third.getStart() && second.getStart() < third.getStart()) 
+				{
+					return false;
+				}
+			}
+		}
+		return true;
+		
+		 //replace me with actual logic please!
 	}
 	public static void main(String[] args) {
 		ArrayList<MeetingInterval> meet = new ArrayList<MeetingInterval>();
@@ -18,5 +30,9 @@ public class CanAttend {
 		}
 		System.out.println(canAttend(meet) ? "There is no conflict with attending "+meet+" meetings":"Can't attend "+meet+" meetings due to conflict");
 		//please include additional testing here!
+		meet.clear();
+		meet.add(new MeetingInterval(0, 30));
+		meet.add(new MeetingInterval(50,100));
+		meet.add(new MeetingInterval(10, 40));
 	}
 }
